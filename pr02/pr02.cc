1b120a4adadf8a8363eb1489b35a7f96308e2cf1
Rakesh Ramesh <rakesh@vitaly.viz.tamu.edu>
2013-03-05 23:46:50 -0600

    multiple lights working

--- pr02/pr02.cc
+++ pr02/pr02.cc
@@ -29,9 +29,9 @@ class SpotLight
 };
 
 Point3f Pe(0,0,0);      //camera or eye position
-SpotLight spotLight(Point3f(0,100,65),Point3f(0,0,1),160.0/180.0);
+SpotLight spotLight(Point3f(0,14,25),Point3f(0,0,1),160.0/180.0);
 
-Point3f PL = spotLight.source;
+//Point3f PL = spotLight.source;
 Point3f DirectionLight(0,-1,0);
 
 /********************************************* MAIN ****************************************************************/
@@ -67,47 +67,55 @@ int main (int argc, char const* argv[])
 	//vector<Sphere> allSpheres;
 	//allSpheres.pb(sphere1);
 	
-	Sphere sphere1(Point3f(0,0,65),10, Color(1,0.8,0),1);
-	//Sphere sphere2(Point3f(-3,-2,10),3, Color(1,0.2,0.7),2);
+	Sphere sphere1(Point3f(0,0,25),10, Color(1,0.8,0),1);
+	Sphere sphere2(Point3f(-3,-2,10),3, Color(1,0.2,0.7),2);
 	//Sphere sphere3(Point3f(-1,5,8),3, Color(0.1,0.5,1),3);
 	//Sphere sphere4(Point3f(1,1,6),3, Color(0,0.5,1),4);
 	
-	//Plane plane1(Point3f(0,-1,0), Point3f(0,30,0), Color(.3,0,.3));
-	//Plane plane2(Point3f(-1,0,0), Point3f(30,0,0), Color(.3,1,.3));
-	//Plane plane3(Point3f(0,0,-1), Point3f(0,0,60), Color(.3,1,1));
-	//Plane plane4(Point3f(0,0,1), Point3f(0,0,-30), Color(0,1,0));
+	Plane plane1(Point3f(0,-1,0), Point3f(0,30,0), Color(.3,0,.3));
+	Plane plane2(Point3f(-1,0,0), Point3f(30,0,0), Color(.3,1,.3));
+	Plane plane3(Point3f(0,0,-1), Point3f(0,0,60), Color(.3,1,1));
+	Plane plane4(Point3f(0,0,1), Point3f(0,0,-30), Color(0,1,0));
 	Plane plane5(Point3f(0,1,0), Point3f(0,-30,0), Color(1,0,0));
-	//Plane plane6(Point3f(1,0,0), Point3f(-30,0,0), Color(1,1,0));
+	Plane plane6(Point3f(1,0,0), Point3f(-30,0,0), Color(1,1,0));
 
 	vector<Object*> allObjects;
-	allObjects.push_back(dynamic_cast<Object*>(&sphere1));
-	//allObjects.push_back(dynamic_cast<Object*>(&sphere2));
+	//allObjects.push_back(dynamic_cast<Object*>(&sphere1));
+	allObjects.push_back(dynamic_cast<Object*>(&sphere2));
 	//allObjects.push_back(dynamic_cast<Object*>(&sphere3));
 	//allObjects.push_back(dynamic_cast<Object*>(&sphere4));
 	//
-	//allObjects.push_back(dynamic_cast<Object*>(&plane1));
-	//allObjects.push_back(dynamic_cast<Object*>(&plane2));
-	//allObjects.push_back(dynamic_cast<Object*>(&plane3));
-	//allObjects.push_back(dynamic_cast<Object*>(&plane4));
+	allObjects.push_back(dynamic_cast<Object*>(&plane1));
+	allObjects.push_back(dynamic_cast<Object*>(&plane2));
+	allObjects.push_back(dynamic_cast<Object*>(&plane3));
+	allObjects.push_back(dynamic_cast<Object*>(&plane4));
 	allObjects.push_back(dynamic_cast<Object*>(&plane5));
-	//allObjects.push_back(dynamic_cast<Object*>(&plane6));
+	allObjects.push_back(dynamic_cast<Object*>(&plane6));
 	Ray myray(Pe,npe);
 	int No=allObjects.size();	
 	vector<Point3f > myinter;
 	float rnd;
+        Point3f PL;
 	// CHECK IF YOU'RE INSIDE ANY OF THE SPHERES ///////////////////////////
 	for (int i = 0; i < allObjects.size(); i++)
-	{
-		if(!allObjects[i]->isEyeOutside(Pe) && (!allObjects[i]->isEyeOutside(PL)) )
+	{	
+          if(!allObjects[i]->isEyeOutside(Pe) )
 		{	cout<<"Eye is inside the object \n";return 1;}
 	}
 	cout<<"EEye and light are outside all the object \n";
 	
-        double alpha0= cos(spotLight.angle);
+        vector<Point3f> lights;
+        lights.pb(Point3f(10,24,30));
+        lights.pb(Point3f(-10,24,30));
+        lights.pb(Point3f(0,24,0));
 
+        double alpha0= cos(spotLight.angle);
+        int softShadowFlag=0;
 	vector <Point3f> tmp;
 	//tmp.resize(2);
-	Color finalColor;
+	Color finalColor(0,0,0);
+        Color TotalfinalColor(0,0,0);
+        double intensity=1.0/(lights.size());
 	Point3f interPoint(0,0,0);
 	int spNum=0;
         double alpha=0;
@@ -149,145 +157,156 @@ int main (int argc, char const* argv[])
 			Point3f shadowInter,rayStart = tmp[winIndex];
 			int flag=0;
                         Point3f NH ;
-                        Ray jujuRay(rayStart , PL-rayStart);
-
-			flag=0;
-			tmp.clear();
-			double shadowDist=0.0,distToPL=(PL-rayStart).Length(),ratio=1.0,o=1,maxDark=1;
-			int power=1;
-			double cDirect;
-			//*/ FOR SHARP SHADOWS 
-			// * get all other intersection points with jujuRay except for the object itself (winIndex)
-			for(int j=0;j<allObjects.size();j++)
-			{
-				if(j==winIndex)	continue;
-				// for soft shadows, find the distance between the intersection points also
-				if(allObjects[j]->objectName=="Sphere" )	
-				{
-					shadowDist += (((Sphere*)allObjects[j])->getTwoDelta(jujuRay))/(2*( ((Sphere*)allObjects[j])->radius)) ;
-					shadowDist = min(maxDark, shadowDist);
-				}
-				shadowInter = allObjects[j]->getIntersectionPoint(jujuRay);
-				tmp.pb(shadowInter);
-
-			}
-                        int mywinIndex= findWinningPointIndex(tmp , rayStart);
-			if(mywinIndex !=-1 && allObjects[mywinIndex]->objectName=="Sphere")	
-			{
-                          NH  = (tmp[mywinIndex] - ((Sphere*)(allObjects[mywinIndex]))->center   );
-                        }
-//*
-                        else if(mywinIndex !=-1 && allObjects[mywinIndex]->objectName=="Plane")	
-			{
-                          NH = (-1)*(((Plane*)(allObjects[mywinIndex]))->normalVector);
-                        }
+                        finalColor = Color(0,0,0);
 
 
-                        NH.Normalize();
-                        DirectionLight.Normalize();
-                        cDirect = DirectionLight%NH;                         // for directional light control value
-                        //cout<<cDirect<<endl;
-                        if(cDirect < 0.0 )
-                        {
-                          //cout<<cDirect<<endl;
-                          cDirect = 1;
-                        }
-                        else {
-                          //cout<<cDirect<<endl;
-                          cDirect = 0;
-                        }
 
-                        // */
-                        // uncomment the following line for normal point light
-                        //cDirect = 0;
-                        if(mywinIndex !=-1)// && tmp[mywinIndex]!=rayStart)
-			{
-				// if distance between startRay and the point tmp[mywinIndex] < dist b/n startRay and PL then black is the color -> continue
-				double pointToInter = (rayStart-tmp[mywinIndex]).Length(), pointToLight = ( rayStart-PL).Length();
-
-				if( pointToInter < pointToLight)
-				{
-					//ratio =  1 - (shadowDist/distToPL)  ;
-
-					ratio =  1 - pow(shadowDist,power)  ;
-					// uncomment the following line to have sharp shadows
-					//continue;
-
-
-				}
-			}
-			// FOR Soft shadows
-			Color shadowColor(ratio,ratio,ratio);
-			//shadowColor=shadowColor*ratio;
                         
 
-                        alpha = ((rayStart - spotLight.source)%spotLight.direction)/((rayStart - spotLight.source).Length()*(spotLight.direction).Length());
-                        if(alpha >alpha0) alpha=1;
-                        else alpha = 0;
-
-			if(allObjects[winIndex]->objectName=="Sphere")	
-			{
-                                if(spotlightEnabled)
-                                  finalColor =(((Sphere*)allObjects[winIndex])->phongShader(myray))*alpha;
-                                else
-                                  finalColor =((Sphere*)allObjects[winIndex])->phongShader(myray);
-                                
-				if(cDirect==0)
-                                  finalColor = finalColor*shadowColor;
-				/*
-				if(ratio !=1 && mywinIndex!=-1 )
-				{
-
-					Point3f center = ((Sphere*)allObjects[mywinIndex])->center,plpt=(PL - tmp[mywinIndex]),ptnormal=((tmp[mywinIndex]-center));
-					plpt.Normalize();
-					ptnormal.Normalize();
-
-				// merging shadow with the original object's color
-					o = (plpt%ptnormal)+1;
-
-					//finalColor = finalColor*(1-o) + shadowColor*(o);
-
-					//finalColor = finalColor*(shadowColor*o);
-				}
-				*/
-			}
-			if(allObjects[winIndex]->objectName=="Plane")	
-			{
-                                if(spotlightEnabled)
-                                  finalColor =(((Plane*)allObjects[winIndex])->phongShader(myray))*alpha;
-                                else
-                                {
-                                  finalColor =((Plane*)allObjects[winIndex])->lambertShader(myray);
-
-				  //finalColor = ((Plane*)allObjects[winIndex])->getColor();
-                                }
-                                
-
-				//finalColor = ((Plane*)allObjects[winIndex])->getColor();
-				if(cDirect==0)
-                                  finalColor = finalColor*shadowColor;
-				/*
-				if(ratio !=1 && mywinIndex!=-1 )
-				{
-
-					Point3f center = ((Sphere*)allObjects[mywinIndex])->center;
-					Point3f plpt=(PL-tmp[mywinIndex]),ptnormal=((tmp[mywinIndex]-center));
-					plpt.Normalize();
-					ptnormal.Normalize();
-
-					// merging shadow with the original object's color
-					o = (plpt%ptnormal)+1;
-
-					//finalColor = finalColor*(1-o) + shadowColor*(o);
-					//finalColor = finalColor*(shadowColor*o);
-				}
-				// */
+                       TotalfinalColor = Color(0,0,0);
 
 
-				
-			}
+                        int numLights = lights.size();
+                        //cout<<numLights<<endl;
+                        for(int lightNum = 0; lightNum < numLights; lightNum++)
+                        {
+		                  PL = lights[lightNum];
+
+		                  //print(PL);
+		                  finalColor = Color(0,0,0);
+		                  Ray jujuRay(rayStart , PL-rayStart);
+
+		                  flag=0;
+		                  tmp.clear();
+		                  double shadowDist=0.0,distToPL=(PL-rayStart).Length(),ratio=1.0,o=1,maxDark=1;
+		                  int power=1;
+		                  double cDirect;
+		                  //*/ FOR SHARP SHADOWS 
+		                  // * get all other intersection points with jujuRay except for the object itself (winIndex)
+		                  for(int j=0;j<allObjects.size();j++)
+		                  {
+		                    if(j==winIndex)	continue;
+		                    // for soft shadows, find the distance between the intersection points also
+		                    if(allObjects[j]->objectName=="Sphere" )	
+		                    {
+		                      shadowDist += (((Sphere*)allObjects[j])->getTwoDelta(jujuRay))/(2*( ((Sphere*)allObjects[j])->radius)) ;
+		                      shadowDist = min(maxDark, shadowDist);
+		                    }
+		                    shadowInter = allObjects[j]->getIntersectionPoint(jujuRay);
+		                    tmp.pb(shadowInter);
+
+		                  }
+		                  int mywinIndex= findWinningPointIndex(tmp , rayStart);
+		                  if(mywinIndex !=-1 && allObjects[mywinIndex]->objectName=="Sphere")	
+		                  {
+		                    NH  = (tmp[mywinIndex] - ((Sphere*)(allObjects[mywinIndex]))->center   );
+		                  }
+		                  //*
+		                  else if(mywinIndex !=-1 && allObjects[mywinIndex]->objectName=="Plane")	
+		                  {
+		                    NH = (-1)*(((Plane*)(allObjects[mywinIndex]))->normalVector);
+		                  }
 
 
+		                  NH.Normalize();
+		                  DirectionLight.Normalize();
+		                  cDirect = DirectionLight%NH;                         // for directional light control value
+		                  //cout<<cDirect<<endl;
+		                  if(cDirect < 0.0 )
+		                  {
+		                    //cout<<cDirect<<endl;
+		                    cDirect = 1;
+		                  }
+		                  else {
+		                    //cout<<cDirect<<endl;
+		                    cDirect = 0;
+		                  }
+
+		                  // */
+		                  // uncomment the following line for normal point light
+		                  cDirect = 0;
+		                  if(mywinIndex !=-1)// && tmp[mywinIndex]!=rayStart)
+		                  {
+		                    // if distance between startRay and the point tmp[mywinIndex] < dist b/n startRay and PL then black is the color -> continue
+		                    double pointToInter = (rayStart-tmp[mywinIndex]).Length(), pointToLight = ( rayStart-PL).Length();
+
+		                    if( pointToInter < pointToLight)
+		                    {
+		                      //ratio =  1 - (shadowDist/distToPL)  ;
+
+		                      ratio =  1 - pow(shadowDist,power)  ;
+		                      // comment the following line to have sharp shadows
+		                      softShadowFlag = 1;
+
+
+		                    }
+		                  }
+		                  // FOR Soft shadows
+		                  Color shadowColor(ratio,ratio,ratio);
+		                  //shadowColor=shadowColor*ratio;
+
+
+		                  alpha = ((rayStart - spotLight.source)%spotLight.direction)/((rayStart - spotLight.source).Length()*(spotLight.direction).Length());
+		                  if(alpha >alpha0) alpha=1;
+		                  else alpha = 0;
+
+		                  if(allObjects[winIndex]->objectName=="Sphere")	
+		                  {
+		                    if(spotlightEnabled)
+		                      finalColor = finalColor+ (((Sphere*)allObjects[winIndex])->phongShader(myray,PL))*alpha;
+		                    else
+		                      finalColor = finalColor+ ((Sphere*)allObjects[winIndex])->phongShader(myray,PL);
+
+		                    if(mywinIndex!=-1 && softShadowFlag==0)
+		                      finalColor = finalColor+  Color(0,0,0);
+
+		                    if(cDirect==0)
+		                      finalColor = finalColor*shadowColor;
+
+		                                             }
+		                  if(allObjects[winIndex]->objectName=="Plane")	
+		                  {
+		                    if(spotlightEnabled)
+		                      finalColor = finalColor+ (((Plane*)allObjects[winIndex])->phongShader(myray,PL))*alpha;
+		                    else
+		                    {
+		                      //finalColor = finalColor+ ((Plane*)allObjects[winIndex])->lambertShader(myray,PL);
+
+		                      finalColor = finalColor+  ((Plane*)allObjects[winIndex])->getColor();
+		                    }
+
+
+		                    //finalColor = finalColor+  ((Plane*)allObjects[winIndex])->getColor();
+		                    if(mywinIndex!=-1 && softShadowFlag==0)
+		                      finalColor = finalColor+  Color(0,0,0);
+		                    if(cDirect==0)
+		                      finalColor =  finalColor*shadowColor;
+		                    
+		                    //printf("Final color in iteration %i : \n", lightNum);
+		                    //finalColor.printColor();
+
+
+		                  }
+
+		                  TotalfinalColor = TotalfinalColor + finalColor*intensity;
+                                  //cout<<"total color: "; TotalfinalColor.printColor();cout<<endl;
+                          
+
+                        }
+			finalColor = TotalfinalColor;
+                        /*
+                        if(finalColor.red > 1.0)  finalColor.red = 1.0;
+                        if(finalColor.green > 1.0)  finalColor.green = 1.0;
+                        if(finalColor.blue > 1.0)  finalColor.blue = 1.0;
+
+                        if(finalColor.red < 0)  finalColor.red = 0;
+                        if(finalColor.green < 0)  finalColor.green = 0;
+                        if(finalColor.blue < 0)  finalColor.blue = 0;
+                        // */
+			
+
+			
+                        
 
 			pixels[index].r=finalColor.red;
 			pixels[index].g=finalColor.green;
